4e1d9016aeb3878fed94a2c740a95eda9f7ef574
LuEsPe <luciaesteban.desarrollo@gmail.com>
2025-06-06 19:45:08 +0200

    arreglillo

--- lib/datosDePrueba/tipoDeEventosDePrueba.dart
+++ lib/datosDePrueba/tipoDeEventosDePrueba.dart
@@ -6,22 +6,18 @@ List<TipoTareas> tareasEjemplo() {
   return [
     TipoTareas(
       nombre: "Lavar los platos",
-      descripcion: "Limpiar todos los platos y utensilios después de las comidas",
       puntuacion: 5,
     ),
     TipoTareas(
       nombre: "Aspirar la casa",
-      descripcion: "Pasar la aspiradora en todas las habitaciones",
       puntuacion: 10,
     ),
     TipoTareas(
       nombre: "Sacar la basura",
-      descripcion: "Llevar la basura a los contenedores correspondientes",
       puntuacion: 3,
     ),
     TipoTareas(
       nombre: "Limpiar el baño",
-      descripcion: "Fregar y desinfectar lavabo, inodoro y ducha",
       puntuacion: 8,
     ),
   ];
