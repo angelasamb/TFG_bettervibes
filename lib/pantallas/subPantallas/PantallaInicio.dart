104ef26e2891c39def08a692770c7bbd29ec4c45
angelasamb <ansamba.2005@gmail.com>
2025-06-06 17:25:37 +0200

    Revert "Merge remote-tracking branch 'origin/15-pantalla-inicio' into Desarrollo"
    This reverts commit 4060531ada9dffa7b4d74aec6f355eb65ac9a33f, reversing
    changes made to 5c6eaf866c06e431aaf7b503dd6350544cb69586.

--- lib/pantallas/subPantallas/PantallaInicio.dart
+++ lib/pantallas/subPantallas/PantallaInicio.dart
@@ -1,59 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:tfg_bettervibes/widgets/ListaTareasPorDiaYfinalizacion.dart';
-import 'package:tfg_bettervibes/pantallas/subPantallas/PantallaTODO.dart';
-
-import '../../widgets/ListaTareas.dart';
 
 class PantallaInicio extends StatelessWidget {
-  const PantallaInicio({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final hoy = DateTime.now();
-
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const PantallaTODO()),
-              );
-            },
-            child: const Text(
-              'TAREAS HOY',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.underline,
-              ),
-            ),
-          ),
-          const SizedBox(height: 12),
-
-          SizedBox(
-            height: 300,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Material(
-                elevation: 2,
-                child: Container(
-                  color: Colors.white,
-                  child: Scrollbar(
-                    child: ListaTareasPorDiaYFinalizacion(
-                      fecha: hoy,
-                      mostrarRealizadas: false,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return Center(child: Text('Contenido de Inicio'));
   }
 }
