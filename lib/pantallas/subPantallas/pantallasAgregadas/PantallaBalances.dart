f76c8ff654528c081acbe5f99e50207741cfb01e
angelasamb <ansamba.2005@gmail.com>
2025-06-13 19:24:07 +0200

    padding bien

--- lib/pantallas/subPantallas/pantallasAgregadas/PantallaBalances.dart
+++ lib/pantallas/subPantallas/pantallasAgregadas/PantallaBalances.dart
@@ -41,22 +41,25 @@ class _PantallaBalancesState extends State<PantallaBalances> {
             height: double.infinity,
           ),
           SafeArea(
-            child: FutureBuilder<DocumentReference?>(
-              future: _unidadFamiliarRefFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
+            child: Center(child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 600),
+              child: FutureBuilder<DocumentReference?>(
+                future: _unidadFamiliarRefFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
 
-                if (!snapshot.hasData || snapshot.data == null) {
-                  return const Center(
-                    child: Text("Unidad familiar no encontrada."),
-                  );
-                }
+                  if (!snapshot.hasData || snapshot.data == null) {
+                    return const Center(
+                      child: Text("Unidad familiar no encontrada."),
+                    );
+                  }
 
-                final unidadRef = snapshot.data!;
-                return _contenidoBalances(unidadRef);
-              },
+                  final unidadRef = snapshot.data!;
+                  return _contenidoBalances(unidadRef);
+                },
+              ),)
             ),
           ),
         ],
@@ -161,7 +164,8 @@ class _PantallaBalancesState extends State<PantallaBalances> {
                             final nombres = snapshot.data!;
                             return CheckboxListTile(
                               value: data["hecho"] ?? false,
-                              title: Text("${nombres[0]} debe a ${nombres[1]} ${(data["cantidad"] as num).toStringAsFixed(2)}€",
+                              title: Text(
+                                "${nombres[0]} debe a ${nombres[1]} ${(data["cantidad"] as num).toStringAsFixed(2)}€",
                                 style: TextStyle(fontSize: 16),
                               ),
                               onChanged: (value) async {
@@ -212,7 +216,9 @@ class _PantallaBalancesState extends State<PantallaBalances> {
             const SizedBox(height: 16),
             Card(
               elevation: 4,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
               child: Padding(
                 padding: const EdgeInsets.all(12),
                 child: Column(
@@ -220,47 +226,60 @@ class _PantallaBalancesState extends State<PantallaBalances> {
                   children: [
                     const Text(
                       "Bizums realizados",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     const SizedBox(height: 8),
                     StreamBuilder<QuerySnapshot>(
-                      stream: unidadRef.collection("Bizums").where("hecho", isEqualTo: true).snapshots(),
+                      stream:
+                          unidadRef
+                              .collection("Bizums")
+                              .where("hecho", isEqualTo: true)
+                              .snapshots(),
                       builder: (context, snapshotBizumsHechos) {
-                        if (snapshotBizumsHechos.connectionState == ConnectionState.waiting) {
-                          return const Center(child: CircularProgressIndicator());
+                        if (snapshotBizumsHechos.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
                         }
 
-                        final bizumsHechos = snapshotBizumsHechos.data?.docs ?? [];
+                        final bizumsHechos =
+                            snapshotBizumsHechos.data?.docs ?? [];
 
                         if (bizumsHechos.isEmpty) {
                           return const Text("No hay Bizums realizados aún.");
                         }
-                        final ordenados = List.from(bizumsHechos)
-                          ..sort((a, b) {
-                            final tsA = (a["timestamp"] as Timestamp).toDate();
-                            final tsB = (b["timestamp"] as Timestamp).toDate();
-                            return tsB.compareTo(tsA);
-                          });
+                        final ordenados = List.from(bizumsHechos)..sort((a, b) {
+                          final tsA = (a["timestamp"] as Timestamp).toDate();
+                          final tsB = (b["timestamp"] as Timestamp).toDate();
+                          return tsB.compareTo(tsA);
+                        });
                         return Column(
-                          children: ordenados.map((bizumDoc) {
-                            final data = bizumDoc.data() as Map<String, dynamic>;
-                            return FutureBuilder<List<String>>(
-                              future: obtenerNombresUsuarios(
-                                data["personaPaga"] as DocumentReference,
-                                data["personaRecibe"] as DocumentReference,
-                              ),
-                              builder: (context, snapshot) {
-                                if (!snapshot.hasData) return const SizedBox.shrink();
-                                final nombres = snapshot.data!;
-                                return ListTile(
-                                  title: Text(
-                                    "${nombres[0]} pagó a ${nombres[1]} ${(data["cantidad"] as num).toStringAsFixed(2)}€",
-                                    style: const TextStyle(fontSize: 16),
+                          children:
+                              ordenados.map((bizumDoc) {
+                                final data =
+                                    bizumDoc.data() as Map<String, dynamic>;
+                                return FutureBuilder<List<String>>(
+                                  future: obtenerNombresUsuarios(
+                                    data["personaPaga"] as DocumentReference,
+                                    data["personaRecibe"] as DocumentReference,
                                   ),
+                                  builder: (context, snapshot) {
+                                    if (!snapshot.hasData)
+                                      return const SizedBox.shrink();
+                                    final nombres = snapshot.data!;
+                                    return ListTile(
+                                      title: Text(
+                                        "${nombres[0]} pagó a ${nombres[1]} ${(data["cantidad"] as num).toStringAsFixed(2)}€",
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    );
+                                  },
                                 );
-                              },
-                            );
-                          }).toList(),
+                              }).toList(),
                         );
                       },
                     ),
@@ -268,7 +287,6 @@ class _PantallaBalancesState extends State<PantallaBalances> {
                 ),
               ),
             ),
-
           ],
         );
       },
